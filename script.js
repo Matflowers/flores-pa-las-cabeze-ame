bc3f12f01f1d3cad5ad5f07512758ec7b92b4046
Matflowers <floresosinagam@gmail.com>
2025-09-21 09:32:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-onload = () =>{
+
+window.onload = function() {
+  // Código de la animación de las flores aquí
+  onload = () =>{
         document.body.classList.remove("container");
+          
+  const audio = document.getElementById('miMusica');
+  const boton = document.getElementById('botonMusica');
+
+  boton.addEventListener('click', function() {
+    if (audio.paused) {
+      audio.play();
+      boton.textContent = 'Pausar música';
+    } else {
+      audio.pause();
+      boton.textContent = 'Reproducir música';
+    }
+  });
 };
